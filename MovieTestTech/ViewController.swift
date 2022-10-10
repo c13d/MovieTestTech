6961a4c2c3572bc4b51af9f82bc3384a8883cca4
davin <christophorusdavin@gmail.com>
2022-10-10 08:49:10 +0700

    Initial Commit

---
+++ MovieTestTech/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieTestTech
+//
+//  Created by Christophorus Davin on 10/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
